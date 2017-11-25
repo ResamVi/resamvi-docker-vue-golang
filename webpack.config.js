fe4ef6532b7d62fedb1ae10443f69b9d64f10ce9
Julien Midedji <admin@resamvi.de>
2017-11-25 21:09:59 +0100

    Add linting

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      inject: 'head'
-    })
- ]
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            inject: 'head'
+        })
+    ]
 };
