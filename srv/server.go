48f5db0c98edb9b1628a823bda8a56d8616e178a
Julien Midedji <admin@resamvi.de>
2017-12-15 11:00:45 +0100

    Containers now communicate correctly.

--- srv/server.go
+++ srv/server.go
@@ -2,28 +2,46 @@ package main
 
 import "fmt"
 import "net/http"
+import "log"
+import "gopkg.in/mgo.v2"
+import "time"
 
 func handler(write http.ResponseWriter, reader *http.Request) {
-	write.Header().Set("Access-Control-Allow-Origin", "http://localhost:9001");
+	write.Header().Set("Access-Control-Allow-Origin", "nginx:9001");
 	fmt.Println("Send received");
 }
 
 func main() {
 	
+	fmt.Println("Trying localhost:27017")
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%d Sekunden\n", i)
+		time.Sleep(1 * time.Second);
+	}
+
 	// Connect to mongodb
-	/*session, err := mgo.Dial("localhost")
+	session, err := mgo.Dial("mongodb:27017")
 
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close();
 
+	names, err := session.DatabaseNames();
+
+	fmt.Println();
+
+	for _, name := range names {
+		fmt.Println(name);
+	}
+
 	// Select database
-	collection := session.DB("resamvi").C("entries")
+	//collection := session.DB("resamvi").C("entries")
 
 	if err != nil {
 		log.Fatal(err);
-	}*/
+	}
 
 	http.HandleFunc("/", handler);
 	http.ListenAndServe(":8080", nil);
